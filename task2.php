1095542af050b98959d16cdf07b8c0490bc0977e
hamanaka <kazuki.s1987@gmail.com>
2020-04-21 21:31:49 +0900

    修正commit

--- task2.php
+++ task2.php
@@ -1,29 +1,29 @@
-<php
+<?php
 $name = "hamanaka";
- if ($name = hamanaka );{
- echo "私はあなたの名前です";
+if ($name == "hamanaka" ){
+  echo "私はあなたの名前です";
 } else {
- echo "私はあなたの名前ではありません"；
+  echo "私はあなたの名前ではありません";
 }
 
 $total = 0;
 for ($i = 0; $i <= 10000; $i++) {
    $total += $i;
-echo $total;
 }
+echo $total;
 
-$fruits = array("apple", "grape", "peach", "lemon", "orangre");
-
+$fruits = array("apple", "grape", "peach", "lemon", "orange");
 foreach($fruits as $fruit){
- echo "要素は"　. $fruit;
+ echo "要素は" . $fruit;
  echo "\n";
 }
 
 $start = 5;
 $end = 100;
 
-for($i = $start; $i =< $end: $i++){
+for($i = $start; $i <= $end; $i++){
 if($i % 5 == 0){
- echo $i
+ echo $i;
+ }
 }
 ?>
