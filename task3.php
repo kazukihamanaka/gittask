4d952acb13125967b4a09416eaf99a742d4e76c5
hamanaka <kazuki.s1987@gmail.com>
2020-04-22 16:52:43 +0900

    task3.commit

---
+++ task3.php
@@ -0,0 +1,33 @@
+<?php
+function double($number){
+  $result = $number *2;
+  return $result;
+}
+echo double(10);
+
+function f($a, $b){
+  $result = $a + $b;
+  return $result;
+}
+echo f(10,20);
+
+function　multiply($arr){
+  $result = 1
+  $i = arry(1,3,5,7,9)
+  for($i = 1; $i < $$arr; $i++);
+  $result += $i;
+  rerurn $result;
+}
+echo multiply(arry(1,3,5,7,9));
+
+function max_array($arr){
+ $max_number = $arr[0];
+ foreach($arr as $a){
+ if($max<$array[$i]){
+    $max=$array[$i];
+ }
+}
+return $max_number;
+}
+
+?>
