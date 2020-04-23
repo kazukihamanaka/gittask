0e24a38e93f019776841a7dfc78a45e9dd8c73d3
hamanaka <kazuki.s1987@gmail.com>
2020-04-23 21:36:19 +0900

    ??commit

--- task3.php
+++ task3.php
@@ -1,34 +1,33 @@
 <?php
-function double($number){
+function double($number) {
   $result = $number *2;
   return $result;
 }
 echo double(10);
 
-function f($a, $b){
+function f($a, $b) {
   $result = $a + $b;
   return $result;
 }
 echo f(10,20);
 
-function　multiply($arr){
-  $result = 1;
-  foreach($arr as $a){
-  $result *= $a;
+function　multiply($arr) {
+  $result = 0;
+  for($i =1; $i < count($arr); $i++) {
+  $result *= $arr;
+ }
+  return $result;
 }
-  rerurn $result;
-}
-$a = array(1,3,5,7,9);
-echo multiply(arry);
+echo multiply(array(1,3,5,7,9));
 
-function max_array($arr){
- $max_number = $arr[0];
- foreach($arr as $a){
- if($max<$array[$i]){
-    $max=$array[$i];
+function max_array($arr) {
+  $max_number = $arr[0];
+  foreach($arr as $a) {
+  if($max<$array[$i]) {
+  $max=$array[$i];
  }
 }
-return $max_number;
+ return $max_number;
 }
 
 ?>
